2f9e14fcf7cff1a2b6b4e97b21556478d70f100a
cabsalvador <salvadorcabrera@csus.edu>
2022-05-26 17:07:22 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,20 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Sal on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
